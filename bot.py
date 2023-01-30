3291c11789eeb17a7eaacfecbdf6a84eeaccb4c2
mtp61 <mpauly@college.harvard.edu>
2023-01-30 17:16:08 -0500

    Use standard algebraic notation in prompts

--- bot.py
+++ bot.py
@@ -43,12 +43,17 @@ def get_random_move(board: chess.Board) -> chess.Move:
 def get_model_move(board: chess.Board) -> chess.Move:
     openai.api_key = os.getenv('OPENAI_API_KEY')
 
-    move_history = [m.uci() for m in board.move_stack]
-    legal_moves = [m.uci() for m in board.legal_moves]
+    temp_board = chess.Board()
+    move_history_san = []
+    for m in board.move_stack:
+        move_history_san.append(temp_board.san(m))
+        temp_board.push(m)
+    legal_moves_san = [board.san(m) for m in board.legal_moves]
+
     prompt = 'You are a chess engine. Given move history:\n' + \
-        ', '.join(move_history) + '\n' + \
+        ', '.join(move_history_san) + '\n' + \
         'And possible moves:\n' + \
-        ', '.join(legal_moves) + '\n' + \
+        ', '.join(legal_moves_san) + '\n' + \
         'Output the move that maximizes the probability of winning.'
 
     completion_params = {
@@ -62,25 +67,43 @@ def get_model_move(board: chess.Board) -> chess.Move:
     }
     response = openai.Completion.create(**completion_params)
     response_text = response['choices'][0]['text'].lower()
-    response_moves = [m for m in legal_moves if m in response_text]
+    legal_moves_lower = [m.lower() for m in legal_moves_san]
+    response_moves = [list(board.legal_moves)[i]
+                      for i, m in enumerate(legal_moves_lower)
+                      if m in response_text]
+
+    # TODO: find a less hacky way of extracting the response moves
+    # remove response moves that are a subset of other moves
+    repeat_ixs = []
+    response_move_strs = [m for m in legal_moves_lower if m in response_text]
+    for i, s1 in enumerate(response_move_strs):
+        for s2 in response_move_strs[:i] + response_move_strs[i+1:]:
+            if s1 in s2:
+                repeat_ixs.append(i)
+                break
+    response_moves = [m for i, m in enumerate(response_moves) if i not in repeat_ixs]
 
     request_log_append({
         'completion_params': completion_params,
+        'move_history_san': move_history_san,
+        'legal_moves_san': legal_moves_san,
         'response': response,
-        'response_moves': response_moves,
     })
 
-    print(response_moves)  # TODO
+    # TODO: better logging
+    print(move_history_san)
+    print(legal_moves_san)
 
     if len(response_moves) == 0:
         move = get_random_move(board)
-        print(f'Response contained no moves, made random move {move}')
+        print(f'Response contained no moves, made random move {board.san(move)}')
     elif len(response_moves) == 1:
-        move = chess.Move.from_uci(response_moves[0])
-        print(f'Response contained exactly one move {move}')
+        move = response_moves[0]
+        print(f'Response contained exactly one move {board.san(move)}')
     else:
-        move = chess.Move.from_uci(random.choice(response_moves))
-        print(f'Response contained moves {response_moves}, randomly chose move {move}')
+        move = random.choice(response_moves)
+        response_moves_san = [board.san(m) for m in response_moves]
+        print(f'Response contained moves {response_moves_san}, randomly chose move {board.san(move)}')
 
     return move
 
@@ -115,7 +138,7 @@ class Game(threading.Thread):
     def handle_state_change(self, event):
         board = chess.Board()
         for move in event['moves'].split(' '):
-            board.push_san(move)
+            board.push_uci(move)
 
         to_move = 'gpt-chess' if self.color == board.turn else 'Opponent'
         self.log(f'{to_move} to move in position,\n{board_str_unicode(board)}\n')
